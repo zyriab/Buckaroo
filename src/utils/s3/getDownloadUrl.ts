50eb344268042a833b358333a4855186502dc547
Zyriab <flacodevworks@gmail.com>
2022-04-02 20:53:30 +0200

    Cleaned code + minor bug fix

--- src/utils/s3/getDownloadUrl.ts
+++ src/utils/s3/getDownloadUrl.ts
@@ -1,13 +1,7 @@
-import { RequestBody, ReqCommand } from '../../definitions/root';
-import {
-  PutObjectCommand,
-  GetObjectCommand,
-  ServiceOutputTypes,
-} from '@aws-sdk/client-s3';
-import { s3Client } from './s3Client';
+import { RequestBody } from '../../definitions/root';
+import { GetObjectCommand } from '@aws-sdk/client-s3';
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
-import { createPresignedPost, PresignedPost } from '@aws-sdk/s3-presigned-post';
-import { getFileSizeRange, Filetype } from '../tools/getFileSizeRange.utils';
+import { s3Client } from './s3Client';
 import sanitize from 'sanitize-filename';
 import normalize from 'normalize-path';
 
@@ -16,28 +10,23 @@ interface InputArgs {
   fileName: string;
   path: string;
   versionId?: string;
-  fileType?: Filetype;
   root?: boolean;
 }
 
 export async function getDownloadUrl(
   args: InputArgs
-): Promise<
-  | [undefined, string]
-  | [undefined, string, { key: string; value: string }[]]
-  | [Error]
-> {
+): Promise<[undefined, string] | [Error]> {
   try {
     const client = s3Client();
     const dirName = args.root
       ? ''
-      : `${args.req.body.userName}-${args.req.body.userId}`;
+      : `${args.req.body.userName}-${args.req.body.userId}/`;
     const fileName = sanitize(args.fileName);
     const expirationTime = 60 * 1;
 
     let params = {
       Bucket: args.req.body.tenant.bucket.name,
-      Key: `${dirName}/${normalize(args.path)}/${fileName}`,
+      Key: `${dirName}${normalize(args.path)}/${fileName}`,
     };
 
     const res = await getSignedUrl(client, new GetObjectCommand(params), {
