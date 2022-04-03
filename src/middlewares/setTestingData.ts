3dc862683e05880f87dd402ab4c5aaf1fff66a0c
Zyriab <flacodevworks@gmail.com>
2022-04-03 09:47:35 +0200

    Update setTestingData.ts

--- src/middlewares/setTestingData.ts
+++ src/middlewares/setTestingData.ts
@@ -1,19 +1,33 @@
-import { RequestBody, ResponseBody } from "../definitions/root";
+import { RequestBody, ResponseBody } from '../definitions/root';
 
-export function setTestingData(req: RequestBody, res: ResponseBody<any>, next: () => void) {
+export function setTestingData(
+  req: RequestBody,
+  res: ResponseBody<any>,
+  next: () => void
+) {
   const tenant = {
     name: 'test-bucket',
     bucket: {
       exists: true,
       name: `${process.env.BUCKET_NAMESPACE}test-bucket-app`,
-    }
-  }
+    },
+  };
 
   req.body.isAuth = true;
   req.body.tenant = tenant;
-  req.body.userName = 'test-user';
+  req.body.username = 'test-user';
   req.body.userEmail = 'test-user@example.com';
   req.body.userId = '1234abcd';
-  req.body.permissions = ['read:bucket', 'delete:directory'];
+  req.body.permissions = [];
+  
+  if (process.env.TEST_AUTH === 'true')
+    req.body.permissions = [
+      'read:bucket',
+      'delete:directory',
+      'create:file',
+      'update:file',
+      'delete:file',
+    ];
+
   next();
 }
