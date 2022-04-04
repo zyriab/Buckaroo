6ffb167cc0478afbb855564106556b1f66671b77
Zyriab <flacodevworks@gmail.com>
2022-04-04 17:02:16 +0200

    Added test for utility

---
+++ src/__tests__/utils/tools/getFileExtension.test.ts
@@ -0,0 +1,18 @@
+import {getFileExtension} from '../../../utils/tools/getFileExtension.utils';
+
+test('Should return file extension', () => {
+  let result = getFileExtension('example.txt');
+  expect(result).toBe('txt');
+  result = getFileExtension('example.some.txt');
+  expect(result).toBe('txt');
+  
+});
+
+test('Should return an empty stirng', () => {
+  let result = getFileExtension('folder/');
+  expect(result).toBe('');
+  result = getFileExtension('folder.name/');
+  expect(result).toBe('');
+  result = getFileExtension('folder');
+  expect(result).toBe('');
+})
