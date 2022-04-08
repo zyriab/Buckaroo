0b81cf331f645d0ba5d40d764a3a74a3ea520e6f
Zyriab <flacodevworks@gmail.com>
2022-04-08 12:57:23 +0200

    Upgraded upload URL to pre-signed POST URL

--- src/utils/s3/getUploadUrl.ts
+++ src/utils/s3/getUploadUrl.ts
@@ -1,14 +1,20 @@
-import { PutObjectCommand } from '@aws-sdk/client-s3';
-import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
+import {
+  createPresignedPost,
+  PresignedPost,
+  PresignedPostOptions,
+} from '@aws-sdk/s3-presigned-post';
 import sanitize from 'sanitize-filename';
 import normalize from 'normalize-path';
 import formatPath from '../tools/formatPath.utils';
+import getFileSizeRange from '../tools/getFileSizeRange.utils';
 import s3Client from './s3Client';
 import { RequestBody } from '../../definitions/root';
+import { FileType } from '../../definitions/types';
 
 interface InputArgs {
   req: RequestBody;
   fileName: string;
+  fileType: FileType;
   root: string;
   path: string;
 }
@@ -16,25 +22,24 @@ interface InputArgs {
 // TODO: need to use a webhook to remove oldest version once the upload succeded (if + than n versions already exist)
 export default async function getUploadUrl(
   args: InputArgs
-): Promise<[undefined, string] | [Error]> {
+): Promise<[undefined, PresignedPost] | [Error]> {
   try {
-    const client = s3Client();
     const fileName = sanitize(args.fileName);
     const root = normalize(args.root);
     const path = normalize(args.path);
     const fullPath = formatPath(`${root}/${path}/`, { stripTrailing: false });
     const expirationTime = 60 * 0.5;
 
-    const params = {
+    const params: PresignedPostOptions = {
       Bucket: args.req.body.tenant.bucket.name,
       Key: `${fullPath}${fileName}`,
+      Expires: expirationTime,
+      Conditions: [getFileSizeRange(args.fileType)],
     };
 
-    const url = await getSignedUrl(client, new PutObjectCommand(params), {
-      expiresIn: expirationTime,
-    });
+    const presignedPost = await createPresignedPost(s3Client(), params);
 
-    return [undefined, url];
+    return [undefined, presignedPost];
   } catch (err: any) {
     return [err as Error];
   }
