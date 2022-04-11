34bb5859bd420ff72769b970a4781ef9d4629484
Zyriab <flacodevworks@gmail.com>
2022-04-11 07:26:47 +0200

    Commented because useless on Lambda

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-/* eslint-disable no-console */
-import app from './app';
+// /* eslint-disable no-console */
+// import app from './app';
 
-const server = app.listen(process.env.PORT, () =>
-  console.log(`Listening on port ${process.env.PORT}...`)
-);
+// const server = app.listen(process.env.PORT, () =>
+//   console.log(`Listening on port ${process.env.PORT}...`)
+// );
 
-export default server;
+// export default server;
