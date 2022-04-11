d313dd089c7f93a75fd20f04ecd974139c53c4fd
Zyriab <flacodevworks@gmail.com>
2022-04-11 17:42:22 +0200

    Update lambda.ts

--- src/lambda.ts
+++ src/lambda.ts
@@ -1,12 +1,10 @@
-/* eslint-disable import/no-import-module-exports */
+require('source-map-support').install();
 import awsServerlessExpress from 'aws-serverless-express';
 import app from './app';
-// eslint-disable-next-line import/no-extraneous-dependencies
-require('source-map-support').install();
 
 const server = awsServerlessExpress.createServer(app);
 
 exports.handler = (event: any, context: any): any => {
-  console.log('Testing handler')
+  console.log('Testing handler');
   awsServerlessExpress.proxy(server, event, context);
 };
