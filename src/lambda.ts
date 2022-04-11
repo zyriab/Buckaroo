2f66b7092583be9db77d76a938245df5653497ac
Zyriab <flacodevworks@gmail.com>
2022-04-11 06:45:47 +0200

    Create lambda.ts

---
+++ src/lambda.ts
@@ -0,0 +1,11 @@
+/* eslint-disable import/no-import-module-exports */
+import awsServerlessExpress from 'aws-serverless-express';
+import app from './app';
+// eslint-disable-next-line import/no-extraneous-dependencies
+require('source-map-support').install();
+
+const server = awsServerlessExpress.createServer(app);
+
+exports.handler = (event: any, context: any): any => {
+  awsServerlessExpress.proxy(server, event, context);
+};
