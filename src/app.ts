22e99c03cd89e8b20d89dbc53b51ec49ffa7e01e
Zyriab <flacodevworks@gmail.com>
2022-04-12 03:17:47 +0200

    test server

--- src/app.ts
+++ src/app.ts
@@ -1,40 +1,59 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import helmet from 'helmet';
-import dotenv from 'dotenv';
-import { graphqlHTTP } from 'express-graphql';
-import { NoSchemaIntrospectionCustomRule } from 'graphql';
-import { RequestBody, ResponseBody } from './definitions/root';
-import gqlSchema from './graphql/schema/gqlSchema';
-import gqlResolvers from './graphql/resolvers/resolvers';
-import checkAuth from './middlewares/checkAuth';
-import checkBucketExists from './middlewares/checkBucketExists';
-import checkBucketVersioning from './middlewares/checkBucketVersioning';
-import setReqMetadata from './middlewares/setReqMetadata';
-import setTestingData from './middlewares/setTestingData';
-
-const IS_DEV = process.env.NODE_ENV === 'development';
-const IS_TEST = process.env.NODE_ENV === 'test';
-
-// if (IS_DEV) dotenv.config();
-dotenv.config();
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const cors = require('cors');
+const schema = require('./graphql/schema/gqlSchema');
+const resolvers = require('./graphql/resolvers/resolvers');
 
 const app = express();
+app.use(cors());
+app.use(
+  '/gql',
+  graphqlHTTP(async () => ({
+    schema: schema,
+    rootValue: resolvers,
+    graphiql: true,
+  }))
+);
 
-if (!IS_DEV) app.use(helmet());
+module.exports = app;
 
-app.use(bodyParser.json());
+// import express from 'express';
+// import bodyParser from 'body-parser';
+// import helmet from 'helmet';
+// import dotenv from 'dotenv';
+// import { graphqlHTTP } from 'express-graphql';
+// import { NoSchemaIntrospectionCustomRule } from 'graphql';
+// import { RequestBody, ResponseBody } from './definitions/root';
+// import gqlSchema from './graphql/schema/gqlSchema';
+// import gqlResolvers from './graphql/resolvers/resolvers';
+// import checkAuth from './middlewares/checkAuth';
+// import checkBucketExists from './middlewares/checkBucketExists';
+// import checkBucketVersioning from './middlewares/checkBucketVersioning';
+// import setReqMetadata from './middlewares/setReqMetadata';
+// import setTestingData from './middlewares/setTestingData';
 
-// eslint-disable-next-line consistent-return
-app.use((req: RequestBody, res: ResponseBody<any>, next: any) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  if (req.method === 'OPTIONS') {
-    return res.sendStatus(200);
-  }
-  next();
-});
+// const IS_DEV = process.env.NODE_ENV === 'development';
+// const IS_TEST = process.env.NODE_ENV === 'test';
+
+// // if (IS_DEV) dotenv.config();
+// dotenv.config();
+
+// const app = express();
+
+// if (!IS_DEV) app.use(helmet());
+
+// app.use(bodyParser.json());
+
+// // eslint-disable-next-line consistent-return
+// app.use((req: RequestBody, res: ResponseBody<any>, next: any) => {
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
+//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//   if (req.method === 'OPTIONS') {
+//     return res.sendStatus(200);
+//   }
+//   next();
+// });
 
 // if (!IS_TEST) {
 //   app.use(checkAuth);
@@ -45,18 +64,18 @@ app.use((req: RequestBody, res: ResponseBody<any>, next: any) => {
 //   app.use(setTestingData);
 // }
 
-app.get('/health', async (req, res) =>
-  res.status(200).json({ response: 'working successfully' })
-);
+// app.get('/health', async (req, res) =>
+//   res.status(200).json({ response: 'working successfully' })
+// );
 
-app.use(
-  '/gql',
-  graphqlHTTP(async () => ({
-    schema: gqlSchema,
-    rootValue: gqlResolvers,
-    validationRules: [NoSchemaIntrospectionCustomRule],
-    graphiql: IS_DEV,
-  }))
-);
+// app.use(
+//   '/gql',
+//   graphqlHTTP(async () => ({
+//     schema: gqlSchema,
+//     rootValue: gqlResolvers,
+//     validationRules: [NoSchemaIntrospectionCustomRule],
+//     graphiql: IS_DEV,
+//   }))
+// );
 
-export default app;
+// export default app;
