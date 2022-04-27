859368ea5252d9378c54a210eb52dd4d5bbf921f
Zyriab <flacodevworks@gmail.com>
2022-04-27 19:03:42 +0200

    :moneybag: Optimized fn by using HEAD command

--- src/utils/s3/isBucketExisting.ts
+++ src/utils/s3/isBucketExisting.ts
@@ -1,24 +1,24 @@
-import { ListBucketsCommand } from '@aws-sdk/client-s3';
+import { HeadBucketCommand } from '@aws-sdk/client-s3';
 import s3Client from './s3Client';
 
 export default async function isBucketExisting(
   bucketName: string
 ): Promise<[undefined, boolean] | [Error]> {
   try {
-    const data = await s3Client().send(new ListBucketsCommand({}));
+    const data = await s3Client().send(
+      new HeadBucketCommand({ Bucket: bucketName })
+    );
 
-    if (!data.Buckets || data.Buckets!.length === 0) {
-      return [undefined, false];
-    }
-
-    for (const b of data.Buckets) {
-      if (b.Name === bucketName) {
-        return [undefined, true];
-      }
+    if (data.$metadata.httpStatusCode === 200) {
+      return [undefined, true];
     }
 
     return [undefined, false];
   } catch (err) {
+    if ((<any>err).$metadata.httpStatusCode === 301) {
+      return [undefined, false];
+    }
+    
     return [err as Error];
   }
 }
