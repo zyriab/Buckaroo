f10e753ea7bae9beeffb8e685d925e4ae29d1a5f
Zyriab <flacodevworks@gmail.com>
2022-04-28 01:58:59 +0200

    Create tools.utils.ts

---
+++ src/utils/tools.utils.ts
@@ -0,0 +1,11 @@
+import formatPath from './tools/formatPath.utils';
+import getFileExtension from './tools/getFileExtension.utils';
+import getFileSizeRange from './tools/getFileSizeRange.utils';
+import handleErrorResponse from './tools/handleErrorResponse.utils';
+import isDirectory from './tools/isDirectory.utils';
+
+export { formatPath };
+export { getFileExtension };
+export { getFileSizeRange };
+export { handleErrorResponse };
+export { isDirectory };
