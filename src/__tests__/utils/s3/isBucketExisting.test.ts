d374e89e1c4d29c4aaa6af47ae686a93bb107fc0
Zyriab <flacodevworks@gmail.com>
2022-04-27 19:03:47 +0200

    Create isBucketExisting.test.ts

---
+++ src/__tests__/utils/s3/isBucketExisting.test.ts
@@ -0,0 +1,19 @@
+import isBucketExisting from '../../../utils/s3/isBucketExisting';
+import 'dotenv/config';
+
+const bucketName = `${process.env.BUCKET_NAMESPACE}test-bucket-app`;
+beforeAll(() => {
+  process.env.NODE_ENV = 'test';
+});
+
+test('Should check for bucket existence and return true', async () => {
+  const res = await isBucketExisting(bucketName);
+  expect(res[0]).toBeUndefined();
+  expect(res[1]).toBe(true);
+});
+
+test('Should check for bucket existence and return false', async () => {
+  const res = await isBucketExisting('non-existing');
+  expect(res[0]).toBeUndefined();
+  expect(res[1]).toBe(false);
+});
