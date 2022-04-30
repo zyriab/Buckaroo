807d8dfe2eb8b635b85509b9b1dd9573330d08bd
Zyriab <flacodevworks@gmail.com>
2022-04-30 20:07:41 +0200

    Update getAppMetadata.test.ts

--- src/__tests__/utils/auth/getAppMetadata.test.ts
+++ src/__tests__/utils/auth/getAppMetadata.test.ts
@@ -1,10 +1,10 @@
 import { DecodedToken } from '../../../definitions/auth';
 import getAppMetadata from '../../../utils/auth/getAppMetadata';
-import fakeToken from '../../../helpers/mockToken.help';
+import mockToken from '../../../helpers/mockToken.help';
 import 'dotenv/config';
 
 test('Should return App metadata', () => {
-  const token: DecodedToken = { ...fakeToken };
+  const token: DecodedToken = { ...mockToken };
   const result = getAppMetadata(token);
   expect(result).toEqual({ tenant: process.env.ENCRYPTED_TEST_TENANT });
 });
