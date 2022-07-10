1e4f5279ea43bfeb6c2b291270faa5a81e371d0f
Zyriab <flacodevworks@gmail.com>
2022-07-11 01:24:24 +0200

    chore: added more tests

--- src/__tests__/utils/s3/getTextFileContent.test.ts
+++ src/__tests__/utils/s3/getTextFileContent.test.ts
@@ -47,22 +47,23 @@ test('Should return an error (file not found)', async () => {
   const [error, content] = await getTextFileContent({
     fileName,
     root,
-    path,
-    bucketName,
-  });
-
-  expect(error).toBeUndefined();
-  expect(content).toBe('Latest');
-});
-
-test('Should return older text file content', async () => {
-  const [error, content] = await getTextFileContent({
-    fileName,
-    root,
     path: 'somewhere-else/',
     bucketName,
   });
 
   expect(content).toBeUndefined();
   expect(error).not.toBeUndefined();
+});
+
+test('Should return an error (wrong file type)', async () => {
+  const [error, content] = await getTextFileContent({
+    fileName: 'test.png',
+    root,
+    path,
+    bucketName,
+  });
+
+  expect(content).toBeUndefined();
+  expect(error).not.toBeUndefined();
+  expect(error?.message).toContain('is not of type "text/*"');
 });
